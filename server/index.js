968b1c57d94f17c77db267282e7430f1ea1c2f88
Daniel Wang <daniel.wang.1999@hotmail.com>
2018-11-10 01:25:10 -0500

    starting routes

--- server/index.js
+++ server/index.js
@@ -1,1 +1,8 @@
 const express = require('express');
+const app = express(); //in a single project we can have multiple express application, by calling express like a function it generates a new application that representats a running express app
+
+app.get('/', (req, res) =>{
+    res.send({hi: 'there'});
+});
+
+app.listen(5000); //express instructs node to listen to port 5000
