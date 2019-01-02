a0e6aaee65b8b5b5f784080af361083269571f9c
Daniel Wang <daniel.wang.1999@hotmail.com>
2019-01-02 15:38:52 -0500

    updating api credentials

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,7 +1,7 @@
 //dev.js
 module.exports = {
-    googleClientID: "732837763890-t1o05i01k02vncm726j0av3otqhq12o9.apps.googleusercontent.com",
-    googleClientSecret: "z6WaaaQVCKRemPgUFXtvhtCr",
+    googleClientID: "732837763890-8qb20re6vvngarcrm9sd80d06ihgsg98.apps.googleusercontent.com",
+    googleClientSecret: "fx2gsMrnvL26GWbXNe75qKlP",
     mongoURI: "mongodb://localhost/DataCollector",
     cookieKey: 'uqwyihdasjdhfhjwehfbwhbjdbsjfbsjbfqowieqshduqedbrehqk',
     stripePublishableKey: 'pk_test_t673ABQwOBEtKPo39kE7wPdV',
