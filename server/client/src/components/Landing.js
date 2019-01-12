a453f9a8ff8c0b4d04c37d7fd4d3510ff49a00ad
Daniel Wang <daniel.wang.1999@hotmail.com>
2019-01-12 14:40:08 -0500

    design changes

--- server/client/src/components/Landing.js
+++ server/client/src/components/Landing.js
@@ -7,12 +7,12 @@ const Landing = () => {
             <div class="section no-pad-bot" id="index-banner">
                 <div class="container">
                     <br/><br/>
-                        <h1 class="header center white-text">Starter Template</h1>
+                        <h1 class="header center white-text">InfoPlus</h1>
                         <div class="row center ">
-                            <h5 class="header col s12 light white-text">A modern responsive front-end framework based on Material Design</h5>
+                            <h5 class="header col s12 light white-text">A Comprehensive Data Collection Application</h5>
                         </div>
                         <div class="row center">
-                            <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light orange">Get Started</a>
+                            <a href="#!" id="download-button" class="btn-large waves-effect waves-light orange">Learn More</a>
                         </div>
                         <br/> <br/>
 
@@ -25,28 +25,27 @@ const Landing = () => {
                             <div class="row">
                                 <div class="col s12 m4">
                                     <div class="icon-block">
-                                        <h2 class="center white-text">LOL</h2>
-                                        <h5 class="center white-text">Speeds up development</h5>
+                                        <h2 class="center white-text"> <i class="center white-text large material-icons">markunread</i></h2>
+                                        <h5 class="center white-text">Send out Surveys</h5>
 
-                                        <p class="light white-text">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+                                        <p class="light white-text">Quickly and easily create and send out surveys. Securely gather information at a moments notice.</p>
                                     </div>
                                 </div>
 
                                 <div class="col s12 m4">
                                     <div class="icon-block">
-                                        <h2 class="center white-text">LMAO</h2>
-                                        <h5 class="center white-text">User Experience Focused</h5>
-
-                                        <p class="light white-text">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+                                        <h2 class="center white-text"> <i class="center white-text large material-icons">insert_chart</i></h2>
+                                        <h5 class="center white-text">Data Analysis</h5>
+                                        <p class="light white-text">Easily access gathered information. Utilize our data analysis tools to get meaningful statistics.</p>
                                     </div>
                                 </div>
 
                                 <div class="col s12 m4">
                                     <div class="icon-block">
-                                        <h2 class="center light-blue-text white-text">ROFL</h2>
-                                        <h5 class="center white-text">Easy to work with</h5>
+                                        <h2 class="center white-text"> <i class="center white-text large material-icons">border_color</i></h2>
+                                        <h5 class="center white-text">Get Feedback</h5>
 
-                                        <p class="light white-text">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+                                        <p class="light white-text">Get feedback from customers and students. Whether its right after a product launch or exam. See what your clients think!</p>
                                     </div>
                                 </div>
                             </div>
@@ -61,33 +60,15 @@ const Landing = () => {
                             <div class="row">
                                 <div class="col l6 s12">
                                     <h5 class="white-text">Bio</h5>
-                                    <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+                                    <p class="grey-text text-lighten-4">Coming Soon!</p>
 
 
-                                </div>
-                                <div class="col l3 s12">
-                                    <h5 class="white-text">Settings</h5>
-                                    <ul>
-                                        <li><a class="white-text" href="#!">Link 1</a></li>
-                                        <li><a class="white-text" href="#!">Link 2</a></li>
-                                        <li><a class="white-text" href="#!">Link 3</a></li>
-                                        <li><a class="white-text" href="#!">Link 4</a></li>
-                                    </ul>
-                                </div>
-                                <div class="col l3 s12">
-                                    <h5 class="white-text">Connect</h5>
-                                    <ul>
-                                        <li><a class="white-text" href="#!">Link 1</a></li>
-                                        <li><a class="white-text" href="#!">Link 2</a></li>
-                                        <li><a class="white-text" href="#!">Link 3</a></li>
-                                        <li><a class="white-text" href="#!">Link 4</a></li>
-                                    </ul>
                                 </div>
                             </div>
                         </div>
-                        <div class="footer-copyright">
-                            <div class="container">
-                                Developed by <a class="orange-text text-lighten-3" href="http://wang-daniel.com">Daniel Wang</a>
+                        <div class="footer-copyright text-lighten-3 orange darken-4">
+                            <div class="container center">
+                                Developed by <a class="white-text" href="http://wang-daniel.com">Daniel Wang</a>
                             </div>
                         </div>
                     </footer>
