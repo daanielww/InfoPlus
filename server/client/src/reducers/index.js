651c060cad1c2ee239afffedfdfe8991a444a0af
Daniel Wang <daniel.wang.1999@hotmail.com>
2019-02-08 20:49:04 -0500

    surveycomponent

--- server/client/src/reducers/index.js
+++ server/client/src/reducers/index.js
@@ -1,6 +1,9 @@
 import  {combineReducers} from 'redux';
+//changing the name of reduxer to reduxForm
+import {reducer as reduxForm} from 'redux-form' //object destructuring syntax. Retrieving the reducer function from the object exported
 import authReducer from './authReducer';
 
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,   //the key is where the output of the reducer will be stored at in the store.
+    form: reduxForm //need the special key "form"
 })
